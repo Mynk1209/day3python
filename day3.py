5c9ecc70b7227fc595676addf6086ab5c2b3c01a
Mayank Tyagi <mayank.morta1209@gmail.com>
2024-07-31 14:34:23 +0530

    summer internship

---
+++ day3.py
@@ -0,0 +1,24 @@
+# var1 = True
+# var2 = False
+# print(var1,var2)
+# print(type(var1),type(var2))
+
+# ls = [23,24,25,27,28,29,30,1,'jai shre ram ',True]
+# # print(ls)
+# # print(type(ls))
+# var=ls[]
+# print(ls[4:7])
+
+# studant_name_[0] = ["jocker","jack","jugnu","pagl"]
+# student_name .append = ('mayank')
+
+# ls1 = ("A","B","C","D","E")
+# ls2 = ["85","4","34","3,"93","23","32","21"]
+# ls1.reverse()  
+# ls2.short()
+#<<<<<<<<<<<<<<<<<<<   TUPLE   >>>>>>>>>>>>>>>>>>>>>>>>>
+
+# tpl = (25,45,65,34,45,24,35,'upflairs',True,25,2)
+# print(tpl)
+# print(type(tpl))
+#<<<<<<<<<<<<<<<<<<<<<<<<<  SET   >>>>>>>>>>>>>>>>>>>>>>>>
